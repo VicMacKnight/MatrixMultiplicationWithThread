280b62274f4f7160c27e3182e57fca057251f712
Your Name <you@example.com>
2023-05-09 11:27:33 -0300

    correção + prints

--- MatrixMultiplicationWithThread.cpp
+++ MatrixMultiplicationWithThread.cpp
@@ -1,76 +1,72 @@
 #include <iostream>
-#include <thread> 
+#include <thread>
+#include <vector>
 
-//precisa validar se está correto
-void f_multplicacao(bool linha, int tamanho, float **m_a, float **m_b, float **m_c, float **m_d, float **m_e) {
-    if(linha){
-        m_d[i][j] = 0.0;
-        for (int i = linha; i <= tamanho; i++){
-             for ( int j=0; j< tamanho; j++){
-                m_d[i][j] = m_d[i][j] + m_a[i][1] * m_b[1][j];
-            }
-        }
-    } else {
-        m_e[i][j] = 0.0;
-        for (int i = linha; i <= tamanho; i++){
-             for ( int j=0; j< tamanho; j++){
-                m_e[i][j] = m_e[i][j] + m_a[1][j] * m_b[i][1];
-            }
+void f_multplicacao(int tamanho, float **m_a, float **m_b, float **m_c, int n) {
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            m_c[tamanho][i] += m_a[tamanho][j] * m_b[i][j];
         }
     }
 }
 
 int main(int argc, char *argv[]) {
 
-    int n;
-    float **m_a, **m_b, **m_c, **m_d, **m_e;
-
-    if(argc < 2) return -1;
-    n = atoi(argv[1]);
+    int n = 2;
+    float **m_a, **m_b, **m_c;
+    std::vector<std::thread> resultado;
     
     m_a = new float*[n];
     m_b = new float*[n];
     m_c = new float*[n];
-    m_d = new float*[n];
-    m_e = new float*[n];
 
-    for(int i=0; i<n; i++)
-    {
+    for(int i=0; i<n; i++) {
         m_a[i] = new float[n];
         m_b[i] = new float[n];
         m_c[i] = new float[n];
-        m_d[i] = new float[n];
-        m_e[i] = new float[n];
     }
 
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
-        {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
             m_a[i][j] = i+1;
             m_b[i][j] = (i+1) + (j+1);
             m_c[i][j] = 0.0;
         }
-        
-    std::thread thread1(f_multplicacao, true, n, m_a, m_b, m_c, m_d, m_e);
-    std::thread thread2(f_multplicacao, false, n, m_a, m_b, m_c, m_d, m_e);
-
-    thread1.join();
-    thread2.join();
-
-    m_c = new float*[n];
-    for(int i=0; i<n; i++){
-        m_c[i] = new float[n];
-        for(int j=0; j<n; j++){
-            m_c[i][j] = m_d[i][j] + m_e[i][j];
-        }
     }
-
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<n; j++){
+        
+    //Print A
+    std::cout << "Matriz A" << std::endl;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            std::cout << m_a[i][j] << ", ";
+        }
+        std::cout << std::endl;
+    }
+    
+    //Print B
+    std::cout << "Matriz B" << std::endl;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            std::cout << m_b[i][j] << ", ";
+        }
+        std::cout << std::endl;
+    }
+        
+    for(int i=0; i<n; i++) {
+        std::thread threads(f_multplicacao, i, m_a, m_b, m_c, n);
+        resultado.emplace_back(threads);
+    }
+    
+    for(int i=0; i<n; i++) {
+        resultado[i].join();
+    }
+        
+    std::cout << "Matriz C" << std::endl;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
             std::cout << m_c[i][j] << ", ";
         }
         std::cout << std::endl;
-    };
+    }
     return 0;
 }
